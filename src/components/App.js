1e4e981dedbcccc5237fff9390bb1e7e268feade
Santiago Leiva <santiagoeleiva@gmail.com>
2019-05-26 15:07:09 -0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <section className="hero is-fullheight higo-gradient-bg">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 className="title has-text-white large-font-size"><span className="has-text-grey-lighter">hi</span>go</h1>
+            <h2 className="subtitle has-text-white">Sitio en construcci&oacute;n</h2>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default App;
