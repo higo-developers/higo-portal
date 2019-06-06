33bfec9d0a0e2afbad8768b81d723fe4ffebc4aa
Santiago Leiva <santiagoeleiva@gmail.com>
2019-06-06 14:46:41 -0300

    Se agrega selector de fecha y hora. Se agrega validación de fecha desde y hasta.

--- src/components/vehicle/VehicleSearchForm.js
+++ src/components/vehicle/VehicleSearchForm.js
@@ -1,6 +1,53 @@
 import React from 'react';
+import DateTimePicker from 'react-datetime-picker';
 
 export default class VehicleSearchForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            minDesde: new Date(),
+            fechaDesde: undefined,
+            fechaHasta: undefined,
+        }
+    }
+
+    handleFechaDesde = (fechaDesde) => {
+        this.setState({ fechaDesde: fechaDesde });
+
+        if (!fechaDesde) this.setState({ fechaHasta: undefined });
+    };
+
+    handleFechaHasta = (fechaHasta) => {
+        this.setState({ fechaHasta: fechaHasta });
+    };
+
+    getMinHasta = () => {
+        return (this.state.fechaDesde) ? new Date(this.state.fechaDesde.getTime()) : new Date(this.state.minDesde.getTime());
+    };
+
+    getMaxHasta = () => {
+        let maxHasta = this.getMinHasta();
+        maxHasta.setDate(maxHasta.getDate() + 30);
+        return maxHasta;
+    };
+
+    disableFechaHasta = () => {
+        return this.state.fechaDesde === null || this.state.fechaDesde === undefined;
+    };
+
+    handleClick = () => {
+      console.log(this.state);
+    };
+
+    searchButtonIsDisabled = () => {
+        let invalidFechaDesde = this.state.fechaDesde === null || this.state.fechaDesde === undefined;
+        let invalidFechaHasta = this.state.fechaHasta === null || this.state.fechaHasta === undefined;
+
+        return invalidFechaDesde || invalidFechaHasta;
+    };
+
     render() {
         return (
             <React.Fragment>
@@ -10,7 +57,7 @@ export default class VehicleSearchForm extends React.Component {
                             <div className="field">
                                 <label className="label">Fecha desde</label>
                                 <div className="control">
-                                    <input name="fechaDesde" className="input" type="text" placeholder="Fecha desde"/>
+                                    <DateTimePicker name="fechaDesde" className="input" onChange={this.handleFechaDesde} minDate={this.state.minDesde} value={this.state.fechaDesde} />
                                 </div>
                             </div>
                         </div>
@@ -19,7 +66,7 @@ export default class VehicleSearchForm extends React.Component {
                             <div className="field">
                                 <label className="label">Fecha hasta</label>
                                 <div className="control">
-                                    <input name="fechaHasta" className="input" type="text" placeholder="Fecha hasta"/>
+                                    <DateTimePicker name="fechaHasta" className="input" disabled={this.disableFechaHasta()} onChange={this.handleFechaHasta} minDate={this.getMinHasta()} maxDate={this.getMaxHasta()} value={this.state.fechaHasta} />
                                 </div>
                             </div>
                         </div>
@@ -37,7 +84,7 @@ export default class VehicleSearchForm extends React.Component {
                             <div className="field">
                                 <label className="label is-hidden-mobile">&nbsp;</label>
                                 <div className="control">
-                                    <button name="Confirm" type="button" className="button is-light is-fullwidth">Buscar</button>
+                                    <button name="Confirm" type="button" className="button is-light is-fullwidth" disabled={this.searchButtonIsDisabled()} onClick={this.handleClick}>Buscar</button>
                                 </div>
                             </div>
                         </div>
