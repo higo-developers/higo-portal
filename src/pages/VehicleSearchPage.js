0b20f6aeb1185787a631969bc598115588e69ee7
Santiago Leiva <santiagoeleiva@gmail.com>
2019-06-06 12:02:53 -0300

    Se agrega Navbar

--- src/pages/VehicleSearchPage.js
+++ src/pages/VehicleSearchPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import VehicleSearchForm from "../components/vehicle/VehicleSearchForm";
+import Navbar from "../components/layout/Navbar";
 
 export default class VehicleSearchPage extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <Navbar/>
                 <div className="section higo-bg">
                     <div className="container">
                         <div className="box padding-2">
