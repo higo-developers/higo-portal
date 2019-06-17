df8bd13379dd052066d5ad65be896f5e320db531
Santiago Leiva <santiagoeleiva@gmail.com>
2019-06-17 03:36:20 -0300

    Estructura de formulario de login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
 export default class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
-        return "Login";
+        return (
+            <React.Fragment>
+                <div className="container">
+                    <form>
+                        <div className="field">
+                            <label className="label">Name</label>
+                            <div className="control">
+                                <input className="input" type="email" name="email" placeholder="E-Mail"/>
+                            </div>
+                        </div>
+
+                        <div className="field">
+                            <label className="label">Password</label>
+                            <div className="control">
+                                <input className="input" type="password" name="password" placeholder="Password"/>
+                            </div>
+                        </div>
+
+                        <div className="field">
+                            <div className="control">
+                                <button className="button is-dark is-fullwidth">Confirmar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </React.Fragment>
+        );
     }
 }
