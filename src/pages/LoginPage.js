7ae9117ac5fd888762ff064a8e072af20149b807
Santiago Leiva <santiagoeleiva@gmail.com>
2019-06-17 03:54:49 -0300

    Agrega handleChange para inputs de formulario

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,12 +3,23 @@ import React from 'react';
 export default class LoginPage extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            email: undefined,
+            password: undefined
+        };
     }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
 
     render() {
         return (
             <React.Fragment>
-                <section className="hero is-fullheight-with-navbar has-background-light">
+                <section className="hero is-light is-bold is-fullheight-with-navbar">
                     <div className="hero-body">
                         <div className="container">
                             <div className="columns is-centered">
@@ -18,14 +29,14 @@ export default class LoginPage extends React.Component {
                                             <div className="field">
                                                 <label className="label">E-Mail</label>
                                                 <div className="control">
-                                                    <input className="input" type="email" name="email" placeholder="E-Mail"/>
+                                                    <input className="input" type="email" name="email" placeholder="E-Mail" onChange={this.handleChange} value={this.state.email}/>
                                                 </div>
                                             </div>
 
                                             <div className="field">
                                                 <label className="label">Password</label>
                                                 <div className="control">
-                                                    <input className="input" type="password" name="password" placeholder="Password"/>
+                                                    <input className="input" type="password" name="password" placeholder="Password" onChange={this.handleChange} value={this.state.password} />
                                                 </div>
                                             </div>
 
