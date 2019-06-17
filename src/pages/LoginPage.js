2917dcd25ee3951c091cd033e537235022be151f
Santiago Leiva <santiagoeleiva@gmail.com>
2019-06-17 03:47:37 -0300

    Modifica estilos de pantalla de login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -8,29 +8,41 @@ export default class LoginPage extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div className="container">
-                    <form>
-                        <div className="field">
-                            <label className="label">Name</label>
-                            <div className="control">
-                                <input className="input" type="email" name="email" placeholder="E-Mail"/>
-                            </div>
-                        </div>
+                <section className="hero is-fullheight-with-navbar has-background-light">
+                    <div className="hero-body">
+                        <div className="container">
+                            <div className="columns is-centered">
+                                <div className="column is-half">
+                                    <div className="box padding-2">
+                                        <form>
+                                            <div className="field">
+                                                <label className="label">E-Mail</label>
+                                                <div className="control">
+                                                    <input className="input" type="email" name="email" placeholder="E-Mail"/>
+                                                </div>
+                                            </div>
 
-                        <div className="field">
-                            <label className="label">Password</label>
-                            <div className="control">
-                                <input className="input" type="password" name="password" placeholder="Password"/>
-                            </div>
-                        </div>
+                                            <div className="field">
+                                                <label className="label">Password</label>
+                                                <div className="control">
+                                                    <input className="input" type="password" name="password" placeholder="Password"/>
+                                                </div>
+                                            </div>
 
-                        <div className="field">
-                            <div className="control">
-                                <button className="button is-dark is-fullwidth">Confirmar</button>
+                                            <div className="field">
+                                                <div className="control">
+                                                    <button
+                                                        className="button is-dark is-fullwidth is-medium">Confirmar
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </section>
             </React.Fragment>
         );
     }
