5476b0689effb8b915f797e72b56ac4227812139
Santiago Leiva <santiagoeleiva@gmail.com>
2019-06-18 11:04:03 -0300

    Depreca Layout. Coloca Navbar directamente en App.

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,24 @@
 import React from "react";
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import VehicleSearchPage from "../pages/VehicleSearchPage";
-import Layout from "./layout/Layout";
 import NotFoundPage from "../pages/NotFoundPage";
 import FilteredVehiclesPage from "../pages/FilteredVehiclesPage";
 import LoginPage from "../pages/LoginPage";
 import VehicleDetailPage from "../pages/VehicleDetailPage";
+import Navbar from "./layout/Navbar";
 
 export default function App() {
     return (
         <BrowserRouter>
-            <Layout>
-                <Switch>
-                    <Route exact path="/" render={() => <Redirect to="/search"/>}/>
-                    <Route exact path="/login" component={LoginPage}/>
-                    <Route exact path="/search" component={VehicleSearchPage}/>
-                    <Route exact path="/vehicles" component={FilteredVehiclesPage}/>
-                    <Route exact path="/vehicles/:id" component={VehicleDetailPage}/>
-                    <Route component={NotFoundPage}/>
-                </Switch>
-            </Layout>
+            <Navbar/>
+            <Switch>
+                <Route exact path="/" render={() => <Redirect to="/search"/>}/>
+                <Route exact path="/login" component={LoginPage}/>
+                <Route exact path="/search" component={VehicleSearchPage}/>
+                <Route exact path="/vehicles" component={FilteredVehiclesPage}/>
+                <Route exact path="/vehicles/:id" component={VehicleDetailPage}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
         </BrowserRouter>
     );
 }
