a0b422cdc978cf6b743d4149dd1710cb0fe34b2d
Pablo Bertoli <berto2904@gmail.com>
2019-06-28 20:57:46 -0300

    creacion de formulario para el registro de usuario

--- src/components/authentication/LoginButton.js
+++ src/components/authentication/LoginButton.js
@@ -5,13 +5,13 @@ import { isAuthenticated } from "../../utils/AuthenticationUtils";
 const AuthenticateButtons = withRouter(({history}) => (
     !isAuthenticated() && (
         <React.Fragment>
+            <button className="button is-dark" onClick={() => { history.push("/register") }}>
+                <i className="fas fa-user-plus"></i>&nbsp; Crear cuenta
+            </button>
             <button className="button is-dark" onClick={() => { history.push("/login") }}>
-                <i className="fas fa-sign-in-alt"></i>&nbsp; Ingresar
+                <i className="fas fa-sign-in-alt"></i>&nbsp; Iniciar sesion
             </button>
 
-            <button className="button is-dark" onClick={() => { history.push("/register") }}>
-                <i className="fas fa-user-plus"></i>&nbsp; Registrar
-            </button>
         </React.Fragment>
     )
 ));
---
+++ src/pages/UserRegisterPage.js
@@ -0,0 +1,177 @@
+import React from 'react';
+import UserRegisterResource from "../resources/UserRegisterResource";
+import { isNotNullOrUndefined } from "../utils/Utils";
+
+export default class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: false,
+            error: undefined,
+            formData: {
+                email: "",
+                password: "",
+                nombre:"",
+                apellido:"",
+                dni:"" ,
+                telefono:""
+            },
+            validData:{
+                passwordConfirm:""
+            }
+            
+        };
+
+        this._isMounted = false;
+    }
+
+    componentDidMount() {
+        this._isMounted = true;
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    handleChange = (event) => {
+        const {name, value} = event.target;
+        
+        if (event.target.name == "passwordConfirm") {
+            this.setState({
+                validData:{
+                    ...this.state.validData,
+                    [name]: value
+                }
+            });
+            return
+        }
+
+        this.setState({
+            formData: {
+                ...this.state.formData,
+                [name]: value
+            }
+        });
+    };
+
+    handleClick = async (event) => {
+        event.preventDefault();
+        this.setState({"loading": true, "error": undefined})
+
+        try {
+            let response = await UserRegisterResource.createUser(this.state.formData);
+
+            if (isNotNullOrUndefined(response.errorMessage)) throw new Error(response.errorMessage);
+            this._isMounted && this.setState({"loading": false, "error": undefined});
+        } catch (e) {
+            this.setState({"loading": false, "error": e});
+        }
+    };
+
+    invalidForm = () => {
+        if (!this.state.formData.email || !this.state.formData.password || !this.state.formData.nombre || !this.state.formData.apellido) {
+            return true;
+        }
+        if (this.state.formData.password != this.state.validData.passwordConfirm) {
+            return true
+        }
+        return false;
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <section className="hero is-light is-bold is-fullheight-with-navbar">
+                    <div className="hero-body">
+                        <div className="container">
+                            <div className="columns is-centered">
+                                <div className="column is-half">
+                                    <div className="box padding-3">
+                                        <form>
+                                            <div className="field">
+                                                <label className="label">E-Mail</label>
+                                                <div className="control">
+                                                    <input className="input" type="email" name="email"
+                                                           placeholder="E-Mail" onChange={this.handleChange}
+                                                           value={this.state.formData.email}/>
+                                                </div>
+                                            </div>
+
+                                            <div className="field">
+                                                <label className="label">Password</label>
+                                                <div className="control">
+                                                    <input className="input" type="password" name="password"
+                                                           placeholder="Password" onChange={this.handleChange}
+                                                           value={this.state.formData.password}/>
+                                                </div>
+                                            </div>
+                                            <div className="field">
+                                                <label className="label">Confirma el password</label>
+                                                <div className="control">
+                                                    <input className="input" type="password" name="passwordConfirm"
+                                                           placeholder="Password" onChange={this.handleChange}
+                                                           value={this.state.validData.passwordConfirm}/>
+                                                </div>
+                                            </div>
+                                            <div className="field">
+                                                <label className="label">Nombre</label>
+                                                <div className="control">
+                                                    <input className="input" type="text" name="nombre"
+                                                           placeholder="Nombre" onChange={this.handleChange}
+                                                           value={this.state.formData.nombre}/>
+                                                </div>
+                                            </div>
+                                            <div className="field">
+                                                <label className="label">Apellido</label>
+                                                <div className="control">
+                                                    <input className="input" type="text" name="apellido"
+                                                           placeholder="Apellido" onChange={this.handleChange}
+                                                           value={this.state.formData.apellido}/>
+                                                </div>
+                                            </div>
+                                            <div className="field">
+                                                <label className="label">DNI</label>
+                                                <div className="control">
+                                                    <input className="input" type="text" name="dni"
+                                                           placeholder="DNI" onChange={this.handleChange}
+                                                           value={this.state.formData.dni}/>
+                                                </div>
+                                            </div>
+                                            <div className="field">
+                                                <label className="label">Telefono</label>
+                                                <div className="control">
+                                                    <input className="input" type="tel" name="telefono"
+                                                           placeholder="Telefono" onChange={this.handleChange}
+                                                           value={this.state.formData.telefono}/>
+                                                </div>
+                                            </div>
+
+                                            {
+                                                this.state.error && (
+                                                    <article className="message is-danger">
+                                                        <div className="message-body">{this.state.error.message}</div>
+                                                    </article>
+                                                )
+                                            }
+
+                                            <div className="field">
+                                                <div className="control">
+                                                    <button
+                                                        className={`button is-dark is-fullwidth is-medium ${this.state.loading && 'is-loading'}`}
+                                                        onClick={this.handleClick}
+                                                        disabled={this.invalidForm()}>Registrarse
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </React.Fragment>
+        );
+    }
+}
