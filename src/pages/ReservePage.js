343c0c2e3a72399d467e88814c5bac3ef501b358
Santiago Leiva <santiagoeleiva@gmail.com>
2019-06-28 01:22:29 -0300

    Se agrega página de confirmación de reserva

--- src/pages/ReservePage.js
+++ src/pages/ReservePage.js
@@ -6,10 +6,29 @@ export default class ReservePage extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div className="container">
-                    <h1>Pagina de reserva</h1>
+                <div className="modal is-active">
+                    <div className="modal-background"></div>
+
+                    <div className="modal-content">
+                        <div className="card">
+                            <header className="card-header">
+                                <p className="card-header-title">
+                                    Pagina de reserva
+                                </p>
+                            </header>
+                            <div className="card-content">
+                                <div className="content">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis
+                                    mauris.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <button className="modal-close is-large" aria-label="close" onClick={() => this.props.history.goBack()}></button>
                 </div>
             </React.Fragment>
-        );
+        )
+            ;
     }
 }
