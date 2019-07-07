39a26689f73d8cb1a3830a4b2a65534e52e181d2
Santiago Leiva <santiagoeleiva@gmail.com>
2019-07-07 17:01:38 -0300

    Agrega datos a completar luego de iniciar sesión en FB

--- src/pages/CompleteUserDataPage.js
+++ src/pages/CompleteUserDataPage.js
@@ -1,18 +1,52 @@
 import React from 'react';
+import GoBackButton from "../components/layout/GoBackButton";
+import LocationAutocomplete from "../components/location/LocationAutocomplete";
 
 export default class CompleteUserDataPage extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            loading: false,
             userData: this.props.history.location.state
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChangeQuery = this.onChangeQuery.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.submitButtonIsDisabled = this.submitButtonIsDisabled.bind(this);
     }
+
+    onChangeQuery = (locationData) => {
+        this.setState({
+            userData: {
+                ...this.state.userData,
+                location: locationData
+            }
+        });
+    };
+
+    handleChange = (event) => {
+        const target = event.target;
+
+        const name = target.name;
+        const value = target.value;
+
+        this.setState({
+            userData: {
+                ...this.state.userData,
+                [name]: value
+            }
+        });
+    };
+
+    submitButtonIsDisabled = () => {
+        return !this.state.userData.dni || !this.state.userData.telefono || !this.state.userData.location;
+    };
 
     handleSubmit = (event) => {
         event.preventDefault();
+        this.setState({loading: true});
     };
 
     render() {
@@ -22,17 +56,90 @@ export default class CompleteUserDataPage extends React.Component {
                     <div className="hero-body">
                         <div className="container">
                             <div className="columns is-centered">
-                                <div className="column is-half">
+                                <div className="column is-three-quarters">
                                     <div className="box padding-2">
-                                        <p>Pagina para completar datos de usuario</p>
+
+                                        <div className="columns is-multiline">
+                                            <div className="column is-fullwidth">
+                                                <nav className="level is-mobile">
+                                                    <div className="level-left is-hidden-mobile">
+                                                        <h1 className="title">Completar datos de usuario</h1>
+                                                    </div>
+                                                    <div className="level-right">
+                                                        <div className="level-item">
+                                                            <GoBackButton/>
+                                                        </div>
+                                                    </div>
+                                                </nav>
+                                            </div>
+                                        </div>
 
                                         <form onSubmit={this.handleSubmit}>
-                                            <p>{this.state.userData.nombre}</p>
-                                            <p>{this.state.userData.apellido}</p>
-                                            <p>{this.state.userData.email}</p>
-                                            <p>{this.state.userData.password}</p>
-                                            <p>{this.state.userData.telefono}</p>
-                                            <p>{this.state.userData.dni}</p>
+                                            <div className="columns is-multiline">
+                                                <div className="column is-half">
+                                                    <div className="control">
+                                                        <input className="input" type="text" name="nombre" placeholder="Nombre" onChange={this.handleChange} value={this.state.userData.nombre} disabled/>
+                                                    </div>
+                                                </div>
+                                                <div className="column is-half">
+                                                    <div className="control">
+                                                        <input className="input" type="text" name="apellido" placeholder="Apellido" onChange={this.handleChange} value={this.state.userData.apellido} disabled/>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="columns is-multiline">
+                                                <div className="column is-fullwidth">
+                                                    <input className="input" type="email" name="email" placeholder="E-Mail" onChange={this.handleChange} value={this.state.userData.email} disabled/>
+                                                </div>
+                                            </div>
+
+                                            <hr/>
+
+                                            <div className="columns is-multiline">
+                                                <div className="column is-half">
+                                                    <div className="field">
+                                                        <label className="label">Tel&eacute;fono <span className="has-text-grey-light">*</span></label>
+                                                        <div className="control">
+                                                            <input className="input" type="text" name="telefono" placeholder="Teléfono" onChange={this.handleChange} value={this.state.userData.telefono}/>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="column is-half">
+                                                    <div className="field">
+                                                        <label className="label">DNI <span
+                                                            className="has-text-grey-light">*</span></label>
+                                                        <div className="control">
+                                                            <input className="input" type="number" min={0} name="dni" placeholder="DNI" onChange={this.handleChange} value={this.state.userData.dni}/>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="columns is-multiline">
+                                                <div className="column is-fullwidth">
+                                                    <div className="field">
+                                                        <label className="label">Ubicaci&oacute;n <span className="has-text-grey-light">*</span></label>
+                                                        <div className="control">
+                                                            <LocationAutocomplete onChangeQuery={this.onChangeQuery}/>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="columns is-centered is-multiline">
+                                                <div className="column is-three-quarters">
+                                                    <div className="field">
+                                                        <div className="control">
+                                                            <button type="submit" className={`button is-dark is-fullwidth is-medium ${this.state.loading && 'is-loading'}`} disabled={this.submitButtonIsDisabled()}>
+                                                                <span className="icon"><i className="fas fa-check"/></span>
+                                                                <span>Confirmar</span>
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </form>
                                     </div>
                                 </div>
