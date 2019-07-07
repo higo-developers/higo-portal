d15738ce732031961d2326ad1bd96839e6531a93
Santiago Leiva <santiagoeleiva@gmail.com>
2019-07-07 02:10:46 -0300

    WIP - Refactor location autocomplete

---
+++ src/components/location/LocationAutocomplete.js
@@ -0,0 +1,78 @@
+/* global google */
+
+import React from 'react';
+import Script from "react-load-script";
+import {
+    getAddressComponentTypeName,
+    getAddressComponentValue,
+    validAddressComponents,
+    validAddressComponentType
+} from "../../utils/VehicleSearchUtils";
+
+const ADDRESS_COMPONENTS = "address_components";
+
+export default class LocationAutocomplete extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            locationData: undefined
+        }
+    }
+
+
+    handleChangeQuery = (event) => {
+        let query = event.target.value;
+
+        if (!query)
+            this.props.onChangeQuery(undefined);
+    };
+
+    initializeAutocomplete = () => {
+        let queryInput = document.getElementById("query");
+        let options = {types: ["geocode"]};
+        let autocompleteFields = [ADDRESS_COMPONENTS];
+
+        this.autocomplete = new google.maps.places.Autocomplete(queryInput, options);
+        this.autocomplete.setFields(autocompleteFields);
+        this.autocomplete.addListener("place_changed", this.handlePlaceSelect);
+    };
+
+    handlePlaceSelect = () => {
+        let place = this.autocomplete.getPlace();
+        let addressComponents = place[ADDRESS_COMPONENTS];
+
+        if (validAddressComponents(addressComponents)) {
+            addressComponents.forEach((aComponent) => {
+                this.addAddressComponent(aComponent)
+            });
+        }
+    };
+
+    addAddressComponent = (addressComponent) => {
+        if (validAddressComponentType(addressComponent)) {
+            this.setState({
+                locationData: {
+                    ...this.state.locationData,
+                    [getAddressComponentTypeName(addressComponent)]: getAddressComponentValue(addressComponent)
+                }
+            });
+
+            this.props.onChangeQuery(this.state.locationData);
+        }
+    };
+
+    render() {
+        const mapsScriptUrl = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`;
+
+        return (
+            <React.Fragment>
+                <Script url={mapsScriptUrl} onLoad={this.initializeAutocomplete}/>
+
+                <input name="query" id="query" className="input" type="text" placeholder="Buscar por ciudad o localidad"
+                       onChange={this.handleChangeQuery}/>
+            </React.Fragment>
+        );
+    }
+
+}
--- src/components/vehicle/VehicleSearchForm.js
+++ src/components/vehicle/VehicleSearchForm.js
@@ -1,18 +1,8 @@
-/* global google */
-
 import React from 'react';
 import DateTimePicker from 'react-datetime-picker';
-import Script from 'react-load-script';
 import {isNullOrUndefined} from "../../utils/Utils";
-import {
-    encodePreparedSearchParams,
-    getAddressComponentTypeName,
-    getAddressComponentValue,
-    validAddressComponents,
-    validAddressComponentType
-} from "../../utils/VehicleSearchUtils";
-
-const ADDRESS_COMPONENTS = "address_components";
+import {encodePreparedSearchParams} from "../../utils/VehicleSearchUtils";
+import LocationAutocomplete from "../location/LocationAutocomplete";
 
 export default class VehicleSearchForm extends React.Component {
 
@@ -66,41 +56,8 @@ export default class VehicleSearchForm extends React.Component {
         return invalidFechaDesde || invalidFechaHasta || invalidLocation;
     };
 
-    initializeAutocomplete = () => {
-        let queryInput = document.getElementById("query");
-        let options = { types: ["geocode"] };
-        let autocompleteFields = [ ADDRESS_COMPONENTS ];
-
-        this.autocomplete = new google.maps.places.Autocomplete(queryInput, options);
-        this.autocomplete.setFields(autocompleteFields);
-        this.autocomplete.addListener("place_changed", this.handlePlaceSelect);
-    };
-
-    handlePlaceSelect = () => {
-        let place = this.autocomplete.getPlace();
-        let addressComponents = place[ADDRESS_COMPONENTS];
-
-        if ( validAddressComponents(addressComponents) ) {
-            addressComponents.forEach((aComponent) => { this.addAddressComponent(aComponent) });
-        }
-    };
-
-    addAddressComponent = (addressComponent) => {
-        if (validAddressComponentType(addressComponent)) {
-            this.setState({
-                locacion: {
-                    ...this.state.locacion,
-                    [getAddressComponentTypeName(addressComponent)]: getAddressComponentValue(addressComponent)
-                }
-            });
-        }
-    };
-
-    handleChangeQuery = (event) => {
-        let query = event.target.value;
-
-        if (!query)
-            this.setState({ locacion: undefined });
+    onChangeQuery = (locationData) => {
+        this.setState({locacion: locationData});
     };
 
     handleClick = (event) => {
@@ -109,13 +66,8 @@ export default class VehicleSearchForm extends React.Component {
     };
 
     render() {
-        const mapsScriptUrl = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`;
-
         return (
             <React.Fragment>
-                {this.state.addMapsScript && (
-                    <Script url={mapsScriptUrl} onLoad={this.initializeAutocomplete} />
-                )}
                 <form>
                     <div className="columns is-multiline">
                         <div className="column is-6">
@@ -140,7 +92,7 @@ export default class VehicleSearchForm extends React.Component {
                             <div className="field">
                                 <label className="label">Ubicaci&oacute;n <span className="has-text-grey-light">*</span></label>
                                 <div className="control">
-                                    <input name="query" id="query" className="input" type="text" placeholder="Buscar por ciudad o localidad" onChange={this.handleChangeQuery} />
+                                    <LocationAutocomplete onChangeQuery={this.onChangeQuery}/>
                                 </div>
                             </div>
                         </div>
