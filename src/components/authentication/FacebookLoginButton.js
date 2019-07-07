b6476166f55ae112c0841057bb6e6528a8c9cce3
Santiago Leiva <santiagoeleiva@gmail.com>
2019-07-07 01:20:22 -0300

    Modifica data a obtener de Facebook

--- src/components/authentication/FacebookLoginButton.js
+++ src/components/authentication/FacebookLoginButton.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import FacebookLogin from 'react-facebook-login';
 
-const fields = ["id", "first_name", "last_name", "middle_name", "email", "picture"];
+const fields = [
+    "id",
+    "first_name",
+    "last_name",
+    "middle_name",
+    "email",
+    "picture",
+    "location",
+    "hometown"
+];
+
 const fieldsSeparator = ",";
 
 export default class FacebookLoginButton extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            isLoggedIn: false,
-            userId: "",
-            name: "",
-            email: "",
-            avatar: ""
-        };
 
         this.responseFacebook = this.responseFacebook.bind(this);
     }
@@ -26,16 +28,14 @@ export default class FacebookLoginButton extends React.Component {
     render() {
         return (
             <React.Fragment>
-                {this.state.isLoggedIn && (
-                    <FacebookLogin
-                        appId={process.env.REACT_APP_FACEBOOK_APP_ID}
-                        fields={fields.join(fieldsSeparator)}
-                        callback={this.responseFacebook}
-                        cssClass="button is-link is-fullwidth is-medium"
-                        textButton="&nbsp; Ingresar con Facebook"
-                        icon="fab fa-facebook-square"
-                    />
-                )}
+                <FacebookLogin
+                    appId={process.env.REACT_APP_FACEBOOK_APP_ID}
+                    fields={fields.join(fieldsSeparator)}
+                    callback={this.responseFacebook}
+                    cssClass="button is-link is-fullwidth is-medium"
+                    textButton="&nbsp; Ingresar con Facebook"
+                    icon="fab fa-facebook-square"
+                />
             </React.Fragment>
         );
     }
