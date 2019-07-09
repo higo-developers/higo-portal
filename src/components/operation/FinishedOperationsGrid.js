0692b88fd6061e70c6618c7a48a9fd1d9d3d9e74
Santiago Leiva <santiagoeleiva@gmail.com>
2019-07-09 19:09:54 -0300

    Agrega botones para accionar sobre operaciones

--- src/components/operation/FinishedOperationsGrid.js
+++ src/components/operation/FinishedOperationsGrid.js
@@ -17,19 +17,29 @@ export default class FinishedOperationsGrid extends React.Component {
                                 <th>Desde</th>
                                 <th>Hasta</th>
                                 <th>Estado</th>
+                                <th>&nbsp;</th>
                             </tr>
                         </thead>
+                        <tbody>
                         {this.props.data.map(operation => (
-                            <tbody>
-                                <tr>
-                                    <td>{(this.props.role === OperationRoles.PROVIDER) ? operation.adquirente : operation.prestador}</td>
-                                    <td>{operation.vehiculo}</td>
-                                    <td>{datetimeToDayMonYearHourMin(operation.fechaHoraDesde)}</td>
-                                    <td>{datetimeToDayMonYearHourMin(operation.fechaHoraHasta)}</td>
-                                    <td>{operation.estado}</td>
-                                </tr>
-                            </tbody>
+                            <tr key={operation.idOperacion}>
+                                <td>{(this.props.role === OperationRoles.PROVIDER) ? operation.adquirente : operation.prestador}</td>
+                                <td>{operation.vehiculo}</td>
+                                <td>{datetimeToDayMonYearHourMin(operation.fechaHoraDesde)}</td>
+                                <td>{datetimeToDayMonYearHourMin(operation.fechaHoraHasta)}</td>
+                                <td>{operation.estado}</td>
+                                <td>
+                                    <div className="buttons">
+                                        {operation.proximosEstados.length > 0 && operation.proximosEstados.map(estado => (
+                                            <button key={estado.proximoEstado} className="button is-dark">
+                                                {estado.descripcionAccion}
+                                            </button>
+                                        ))}
+                                    </div>
+                                </td>
+                            </tr>
                         ))}
+                        </tbody>
                     </table>
                 ) : (
                     <div className="section">
--- src/components/operation/OnGoingOperationsGrid.js
+++ src/components/operation/OnGoingOperationsGrid.js
@@ -17,27 +17,29 @@ export default class OnGoingOperationsGrid extends React.Component {
                                 <th>Desde</th>
                                 <th>Hasta</th>
                                 <th>Estado</th>
-                                <th>Acciones</th>
+                                <th>&nbsp;</th>
                             </tr>
                         </thead>
+                        <tbody>
                         {this.props.data.map(operation => (
-                            <tbody>
-                                <tr>
-                                    <td>{(this.props.role === OperationRoles.PROVIDER) ? operation.adquirente : operation.prestador}</td>
-                                    <td>{operation.vehiculo}</td>
-                                    <td>{datetimeToDayMonYearHourMin(operation.fechaHoraDesde)}</td>
-                                    <td>{datetimeToDayMonYearHourMin(operation.fechaHoraHasta)}</td>
-                                    <td>{operation.estado}</td>
-                                    <td>
-                                        <div className="buttons">
-                                            <button className="button is-dark">
-                                                <span><i className="fas fa-check"></i></span>&nbsp; Accion
+                            <tr key={operation.idOperacion}>
+                                <td>{(this.props.role === OperationRoles.PROVIDER) ? operation.adquirente : operation.prestador}</td>
+                                <td>{operation.vehiculo}</td>
+                                <td>{datetimeToDayMonYearHourMin(operation.fechaHoraDesde)}</td>
+                                <td>{datetimeToDayMonYearHourMin(operation.fechaHoraHasta)}</td>
+                                <td>{operation.estado}</td>
+                                <td>
+                                    <div className="buttons">
+                                        {operation.proximosEstados.length > 0 && operation.proximosEstados.map(estado => (
+                                            <button key={estado.proximoEstado} className="button is-dark">
+                                                {estado.descripcionAccion}
                                             </button>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
+                                        ))}
+                                    </div>
+                                </td>
+                            </tr>
                         ))}
+                        </tbody>
                     </table>
                 ) : (
                     <div className="section">
