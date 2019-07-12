47478b462f674e866fa92914c799eba7f91ecc45
Santiago Leiva <santiagoeleiva@gmail.com>
2019-07-11 22:55:17 -0300

    Separa grilla de operaciones de adquirente y prestador en componentes diferentes

---
+++ src/components/operation/AcquirerOperationsGrid.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {datetimeToDayMonYearHourMin} from "../../utils/FormatUtils";
+
+export default class AcquirerOperationsGrid extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <table className="table is-striped is-hoverable is-fullwidth">
+                    <thead>
+                    <tr>
+                        <th>Prestador</th>
+                        <th>Vehiculo</th>
+                        <th>Desde</th>
+                        <th>Hasta</th>
+                        <th>Estado</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.props.data.map(operation => (
+                        <tr key={operation.idOperacion}>
+                            <td>{operation.prestador}</td>
+                            <td>{operation.vehiculo}</td>
+                            <td>{datetimeToDayMonYearHourMin(operation.fechaHoraDesde)}</td>
+                            <td>{datetimeToDayMonYearHourMin(operation.fechaHoraHasta)}</td>
+                            <td>{operation.estado}</td>
+                            <td>
+                                <div className="buttons">
+                                    {operation.proximosEstados.length > 0 && operation.proximosEstados.map(estado => (
+                                        <button key={estado.proximoEstado} className="button is-dark">
+                                            {estado.descripcionAccion}
+                                        </button>
+                                    ))}
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </React.Fragment>
+        );
+    }
+}
--- src/components/operation/OperationsGrid.js
+++ src/components/operation/OperationsGrid.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import {OperationRoles} from "../../utils/Constants";
-import {datetimeToDayMonYearHourMin} from "../../utils/FormatUtils";
-
-const TH_ACQUIRER = "Adquirente";
-const TH_PROVIDER = "Prestador";
+import ProviderOperationsGrid from "./ProviderOperationsGrid";
+import AcquirerOperationsGrid from "./AcquirerOperationsGrid";
 
 export default class OperationsGrid extends React.Component {
     render() {
@@ -12,38 +10,10 @@ export default class OperationsGrid extends React.Component {
         return (
             <React.Fragment>
                 {hasData ? (
-                    <table className="table is-striped is-hoverable is-fullwidth">
-                        <thead>
-                            <tr>
-                                <th>{(this.props.role === OperationRoles.PROVIDER) ? TH_ACQUIRER : TH_PROVIDER}</th>
-                                <th>Vehiculo</th>
-                                <th>Desde</th>
-                                <th>Hasta</th>
-                                <th>Estado</th>
-                                <th>&nbsp;</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {this.props.data.map(operation => (
-                            <tr key={operation.idOperacion}>
-                                <td>{(this.props.role === OperationRoles.PROVIDER) ? operation.adquirente : operation.prestador}</td>
-                                <td>{operation.vehiculo}</td>
-                                <td>{datetimeToDayMonYearHourMin(operation.fechaHoraDesde)}</td>
-                                <td>{datetimeToDayMonYearHourMin(operation.fechaHoraHasta)}</td>
-                                <td>{operation.estado}</td>
-                                <td>
-                                    <div className="buttons">
-                                        {operation.proximosEstados.length > 0 && operation.proximosEstados.map(estado => (
-                                            <button key={estado.proximoEstado} className="button is-dark">
-                                                {estado.descripcionAccion}
-                                            </button>
-                                        ))}
-                                    </div>
-                                </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
+                    (this.props.role === OperationRoles.PROVIDER)
+                        ? <ProviderOperationsGrid data={this.props.data}/>
+                        : <AcquirerOperationsGrid data={this.props.data}/>
+
                 ) : (
                     <div className="section">
                         <p className="has-text-centered">{this.props.emptyDataMessage}</p>
