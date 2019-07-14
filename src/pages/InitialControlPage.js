edb02a4cf82f8107fbda5d3edbe89461beb730be
Santiago Leiva <santiagoeleiva@gmail.com>
2019-07-14 14:34:49 -0300

    Extrae formulario de control en componente aparte

--- src/pages/InitialControlPage.js
+++ src/pages/InitialControlPage.js
@@ -5,6 +5,7 @@ import {FuelLevels, GeneralPerformance, HygieneLevels, Routes} from "../utils/Co
 import OperationResource from "../resources/OperationResource";
 import {handlePossibleErrorResponse} from "../utils/Utils";
 import ChangeOperationStatusModal from "../components/operation/ChangeOperationStatusModal";
+import ControlForm from "../components/operation/control/ControlForm";
 
 export default class InitialControlPage extends React.Component {
     constructor(props) {
@@ -38,15 +39,10 @@ export default class InitialControlPage extends React.Component {
     };
 
     handleChange = (event) => {
-        const target = event.target;
-
-        const value = target.value;
-        const name = target.name;
-
         this.setState({
             control: {
                 ...this.state.control,
-                [name]: value
+                [event.target.name]: event.target.value
             }
         });
     };
@@ -111,104 +107,13 @@ export default class InitialControlPage extends React.Component {
 
                 <section className="section">
                     <div className="container">
-                        <form onSubmit={this.handleSubmit}>
-                            <div className="columns is-multiline">
-                                <div className="column is-one-quarter">
-                                    <div className="columns is-multiline">
-                                        <div className="column is-full">
-                                            <p className="subtitle">Nivel de combustible</p>
-                                        </div>
-                                        <div className="column is-full">
-                                            <div className="field is-grouped-multiline">
-                                                {Object.values(FuelLevels).map((fuelLevel) => {
-                                                    return (
-                                                        <div className="control" key={fuelLevel}>
-                                                            <label className="radio">
-                                                                <input type="radio" name="fuelLevel" checked={fuelLevel === this.state.control.fuelLevel} onChange={this.handleChange} value={fuelLevel} />&nbsp; {fuelLevel}
-                                                            </label>
-                                                        </div>
-                                                    )
-                                                })}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="column is-one-quarter">
-                                    <div className="columns is-multiline">
-                                        <div className="column is-full">
-                                            <p className="subtitle">Higiene externa</p>
-                                        </div>
-                                        <div className="column is-full">
-                                            <div className="field is-grouped-multiline">
-                                                {Object.values(HygieneLevels).map((hygieneLevel) => {
-                                                    return (
-                                                        <div className="control" key={hygieneLevel}>
-                                                            <label className="radio">
-                                                                <input type="radio" name="externalHygieneLevel" checked={hygieneLevel === this.state.control.externalHygieneLevel} onChange={this.handleChange} value={hygieneLevel} />&nbsp; {hygieneLevel}
-                                                            </label>
-                                                        </div>
-                                                    )
-                                                })}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="column is-one-quarter">
-                                    <div className="columns is-multiline">
-                                        <div className="column is-full">
-                                            <p className="subtitle">Higiene interna</p>
-                                        </div>
-                                        <div className="column is-full">
-                                            <div className="field is-grouped-multiline">
-                                                {Object.values(HygieneLevels).map((hygieneLevel) => {
-                                                    return (
-                                                        <div className="control" key={hygieneLevel}>
-                                                            <label className="radio">
-                                                                <input type="radio" name="internalHygieneLevel" checked={hygieneLevel === this.state.control.internalHygieneLevel} onChange={this.handleChange} value={hygieneLevel} />&nbsp; {hygieneLevel}
-                                                            </label>
-                                                        </div>
-                                                    )
-                                                })}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="column is-one-quarter">
-                                    <div className="columns is-multiline">
-                                        <div className="column is-full">
-                                            <p className="subtitle">Funcionamiento general</p>
-                                        </div>
-                                        <div className="column is-full">
-                                            <div className="field is-grouped-multiline">
-                                                {Object.values(GeneralPerformance).map((performance) => {
-                                                    return (
-                                                        <div className="control" key={performance}>
-                                                            <label className="radio">
-                                                                <input type="radio" name="performance" checked={performance === this.state.control.performance} onChange={this.handleChange} value={performance} />&nbsp; {performance}
-                                                            </label>
-                                                        </div>
-                                                    )
-                                                })}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="column is-full">
-                                    <div className="field is-grouped">
-                                        <div className="control">
-                                            <button type="submit" className={`button is-dark ${this.state.loading && 'is-loading'}`} disabled={this.state.disabledSubmitButton}>
-                                                <span className="icon"><i className="fas fa-check" /></span>
-                                                <span>Confirmar</span>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
+                        <ControlForm
+                            control={this.state.control}
+                            loading={this.state.loading}
+                            disabledSubmitButton={this.state.disabledSubmitButton}
+                            onHandleChange={this.handleChange}
+                            onHandleSubmit={this.handleSubmit}
+                        />
                     </div>
                 </section>
 
