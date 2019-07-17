51505b1051e67cc6c426e0a068aec594658f9adc
Santiago Leiva <santiagoeleiva@gmail.com>
2019-07-16 21:12:37 -0300

    Agrega imagen de registro de usuario en modal de detalle

--- src/components/user/UserDetailModal.js
+++ src/components/user/UserDetailModal.js
@@ -3,12 +3,21 @@ import Modal from "../layout/Modal";
 import {locationDataAsArray} from "../../utils/LocationUtils";
 
 const LOCATION_DATA_SEPARATOR = " - ";
+const ENDPOINT_IMAGES = `${process.env.REACT_APP_API_BASE_URL}/imagenes`;
 
 export default function UserDetailModal(props) {
     return (
         props.user
         && <Modal closeModal={props.onCloseModal} modalState={props.modalState} title={props.title}>
             <div className="columns is-multiline">
+                {props.user.pathImagenRegistro && (
+                    <div className="column">
+                        <figure className="image">
+                            <img src={`${ENDPOINT_IMAGES}/${props.user.pathImagenRegistro}`} alt={props.user.nombre}/>
+                        </figure>
+                    </div>
+                )}
+
                 <div className="column is-full">
                     <p className="title">{props.user.nombre}</p>
                 </div>
